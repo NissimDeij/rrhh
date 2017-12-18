edba4d8662cdca2e16c3c3cd7ab4ea8d310fee7e
Patricio <PATRICIO-PC>
2017-12-18 17:48:47 -0300

    PP - Validaciones instantanes agregadas más revisión de funcionalidad

--- rrhh/src/controlador/controlador.java
+++ rrhh/src/controlador/controlador.java
@@ -7,6 +7,8 @@ package controlador;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusListener;
+import java.awt.event.FocusEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import javax.swing.JFrame;
@@ -23,7 +25,7 @@ import vista.menuTrabajador;
  *
  * @author Duoc UC
  */
-public class controlador implements ActionListener, MouseListener {
+public class controlador implements ActionListener, MouseListener, FocusListener {
 
     //vistas
     private menuPrincipal vistaPrincipal;
@@ -43,11 +45,21 @@ public class controlador implements ActionListener, MouseListener {
         itmEliminar, //eliminar trab por codigo
         //botones
         btnBuscar, //buscar un trabajador por codigo
-        btnGuardar, //guardar (o ingresar) nuevo trabajador
+        btnGrabar, //guardar (o ingresar) nuevo trabajador
         btnGuardarCambios, //guardar cambios (al editar trabajador)
         btnEliminar, //eliminar trabajador
         btnLimpiar, //limpiar campos (para agregar o editar trabajador)
-        btnVolver //vuelve a la ventana principal
+        btnVolver, //vuelve a la ventana principal
+        //JTextFields
+        txtCodigo, // campo de ingreso para el código del trabajador
+        txtRut, // campo de ingreso para el rut del trabajador
+        txtDv, // campo de ingreso del dígito verificador para el rut del trabajador
+        txtNombre, // campo de ingreso para el nombre del trabajador
+        txtApellido, // campo de ingreso para el apellido del trabajador
+        txtCelular, // campo de ingreso para el celular del trabajador
+        txtEmail, // campo de ingreso para el email del trabajador
+        txtSueldo, // campo de ingreso para el sueldo del trabajador
+        
     };
 
     //constructor de clase
@@ -63,8 +75,8 @@ public class controlador implements ActionListener, MouseListener {
             SwingUtilities.updateComponentTreeUI(this.vistaTrabajador);
             this.vistaPrincipal.setLocationRelativeTo(null);
             this.vistaPrincipal.setTitle("Human Resources ERP");
-            this.vistaPrincipal.setVisible(true);
             this.vistaPrincipal.tblTrabajadores.setModel(this.modelo.ListarTrabajadores()); //actualiza JTable
+            this.vistaPrincipal.setVisible(true);
             
         } catch (UnsupportedLookAndFeelException ex) {
         } catch (ClassNotFoundException ex) {
@@ -113,6 +125,14 @@ public class controlador implements ActionListener, MouseListener {
         this.vistaTrabajador.btnEliminar.setActionCommand("btnEliminar");
         this.vistaTrabajador.btnEliminar.addActionListener(this);
         
+        //agregamos focus listeners para los campos del formulario ingresar
+        this.vistaTrabajador.txtCodigo.addFocusListener(this);
+        this.vistaTrabajador.txtRut.addFocusListener(this);
+        this.vistaTrabajador.txtNombre.addFocusListener(this);
+        this.vistaTrabajador.txtApellido.addFocusListener(this);
+        this.vistaTrabajador.txtCelular.addFocusListener(this);
+        this.vistaTrabajador.txtEmail.addFocusListener(this);
+        this.vistaTrabajador.txtSueldo.addFocusListener(this);
     }
 
     @Override
@@ -132,7 +152,6 @@ public class controlador implements ActionListener, MouseListener {
                 //habilitamo y deshabilitamos los campos requeridos
                 this.vistaTrabajador.txtCodigo.setEditable(true);
                 this.vistaTrabajador.txtRut.setEditable(false);
-                this.vistaTrabajador.txtDv.setEditable(false);
                 this.vistaTrabajador.txtNombre.setEditable(false);
                 this.vistaTrabajador.txtApellido.setEditable(false);
                 this.vistaTrabajador.txtCelular.setEditable(false);
@@ -156,7 +175,6 @@ public class controlador implements ActionListener, MouseListener {
                 //habilitamo y deshabilitamos los campos requeridos
                 this.vistaTrabajador.txtCodigo.setEditable(true);
                 this.vistaTrabajador.txtRut.setEditable(true);
-                this.vistaTrabajador.txtDv.setEditable(true);
                 this.vistaTrabajador.txtNombre.setEditable(true);
                 this.vistaTrabajador.txtApellido.setEditable(true);
                 this.vistaTrabajador.txtCelular.setEditable(true);
@@ -180,7 +198,6 @@ public class controlador implements ActionListener, MouseListener {
                 //habilitamo y deshabilitamos los campos requeridos
                 this.vistaTrabajador.txtCodigo.setEditable(false);
                 this.vistaTrabajador.txtRut.setEditable(true);
-                this.vistaTrabajador.txtDv.setEditable(true);
                 this.vistaTrabajador.txtNombre.setEditable(true);
                 this.vistaTrabajador.txtApellido.setEditable(true);
                 this.vistaTrabajador.txtCelular.setEditable(true);
@@ -202,8 +219,7 @@ public class controlador implements ActionListener, MouseListener {
                 this.vistaTrabajador.lblTituloVentana.setText("Eliminar trabajador");
                 
                 this.vistaTrabajador.txtCodigo.setEditable(true); //habilitamos solo el campo codigo
-                this.vistaTrabajador.txtRut.setEditable(false);
-                this.vistaTrabajador.txtDv.setEditable(false);
+                this.vistaTrabajador.txtRut.setEditable(false);                
                 this.vistaTrabajador.txtNombre.setEditable(false);
                 this.vistaTrabajador.txtApellido.setEditable(false);
                 this.vistaTrabajador.txtCelular.setEditable(false);
@@ -234,25 +250,23 @@ public class controlador implements ActionListener, MouseListener {
                 this.vistaTrabajador.cmbEstadoCivil.setSelectedItem(String.valueOf(arreglin[6]));
                 this.vistaTrabajador.cmbDepartamento.setSelectedItem(String.valueOf(arreglin[7]));
                 
-            case btnGuardar:
+            case btnGrabar:
                 //Enviamos datos del formulario Agregar Trabajador a metodo agregarTrabajador
                 if (this.modelo.agregarTrabajador(
                         Integer.parseInt(this.vistaTrabajador.txtCodigo.getText()),
                         this.vistaTrabajador.txtRut.getText(),
-                        this.vistaTrabajador.txtDv.getText(),
                         this.vistaTrabajador.txtNombre.getText(),
                         this.vistaTrabajador.txtApellido.getText(),
                         Integer.parseInt(this.vistaTrabajador.txtCelular.getText()),
                         this.vistaTrabajador.txtEmail.getText(),
                         Integer.parseInt(this.vistaTrabajador.txtSueldo.getText()),
-                        this.vistaTrabajador.cmbEstadoCivil.getSelectedItem().toString(),
+                        this.vistaTrabajador.cmbEstadoCivil.getSelectedItem().toString().substring(0, 1),
                         this.vistaTrabajador.cmbDepartamento.getSelectedItem().toString()
                 )) {
                     JOptionPane.showMessageDialog(null, "Trabajador agregado correctamente");
                     //Limpiamos textField
                     this.vistaTrabajador.txtCodigo.setText("");
                     this.vistaTrabajador.txtRut.setText("");
-                    this.vistaTrabajador.txtDv.setText("");
                     this.vistaTrabajador.txtNombre.setText("");
                     this.vistaTrabajador.txtApellido.setText("");
                     this.vistaTrabajador.txtCelular.setText("");
@@ -269,7 +283,6 @@ public class controlador implements ActionListener, MouseListener {
                 if (this.modelo.editarTrabajador(
                         Integer.parseInt(this.vistaTrabajador.txtCodigo.getText()),
                         this.vistaTrabajador.txtRut.getText(),
-                        this.vistaTrabajador.txtDv.getText(),
                         this.vistaTrabajador.txtNombre.getText(),
                         this.vistaTrabajador.txtApellido.getText(),
                         Integer.parseInt(this.vistaTrabajador.txtCelular.getText()),
@@ -283,7 +296,6 @@ public class controlador implements ActionListener, MouseListener {
                     //Limpiamos textField
                     this.vistaTrabajador.txtCodigo.setText("");
                     this.vistaTrabajador.txtRut.setText("");
-                    this.vistaTrabajador.txtDv.setText("");
                     this.vistaTrabajador.txtNombre.setText("");
                     this.vistaTrabajador.txtApellido.setText("");
                     this.vistaTrabajador.txtCelular.setText("");
@@ -304,7 +316,6 @@ public class controlador implements ActionListener, MouseListener {
                     //Limpiamos textField
                     this.vistaTrabajador.txtCodigo.setText("");
                     this.vistaTrabajador.txtRut.setText("");
-                    this.vistaTrabajador.txtDv.setText("");
                     this.vistaTrabajador.txtNombre.setText("");
                     this.vistaTrabajador.txtApellido.setText("");
                     this.vistaTrabajador.txtCelular.setText("");
@@ -320,7 +331,6 @@ public class controlador implements ActionListener, MouseListener {
             case btnLimpiar:
                 this.vistaTrabajador.txtCodigo.setText("");
                 this.vistaTrabajador.txtRut.setText("");
-                this.vistaTrabajador.txtDv.setText("");
                 this.vistaTrabajador.txtNombre.setText("");
                 this.vistaTrabajador.txtApellido.setText("");
                 this.vistaTrabajador.txtCelular.setText("");
@@ -345,7 +355,6 @@ public class controlador implements ActionListener, MouseListener {
             if (fila > -1) {
                 this.vistaTrabajador.txtCodigo.setText(String.valueOf(this.vistaPrincipal.tblTrabajadores.getValueAt(fila, 1)));
                 this.vistaTrabajador.txtRut.setText(String.valueOf(this.vistaPrincipal.tblTrabajadores.getValueAt(fila, 2)));
-                this.vistaTrabajador.txtDv.setText(String.valueOf(this.vistaPrincipal.tblTrabajadores.getValueAt(fila, 2)));
                 this.vistaTrabajador.txtNombre.setText(String.valueOf(this.vistaPrincipal.tblTrabajadores.getValueAt(fila, 3)));
                 this.vistaTrabajador.txtApellido.setText(String.valueOf(this.vistaPrincipal.tblTrabajadores.getValueAt(fila, 4)));
                 this.vistaTrabajador.txtCelular.setText(String.valueOf(this.vistaPrincipal.tblTrabajadores.getValueAt(fila, 5)));
@@ -371,5 +380,82 @@ public class controlador implements ActionListener, MouseListener {
 
     @Override
     public void mouseExited(MouseEvent e) {
+    }
+    @Override
+    public void focusGained(FocusEvent fe) {}
+
+    @Override
+    public void focusLost(FocusEvent fe) {
+        // Validación instantanea al salir del campo código
+        if(fe.getSource()==this.vistaTrabajador.txtCodigo){
+            if(!this.vistaTrabajador.txtCodigo.getText().equals("") && this.vistaTrabajador.lblTituloVentana.getText().equals("Agregar trabajador")){
+                if(!modelo.validarCodigoEntre(this.vistaTrabajador.txtCodigo.getText())){
+                    JOptionPane.showMessageDialog(null, "El código ingresado no es válido.\nPor favor ingrese solo números entre 1 y 100.");
+                    this.vistaTrabajador.txtCodigo.requestFocus();
+                }
+                if(!modelo.validarCodigoExiste(this.vistaTrabajador.txtCodigo.getText())){
+                    JOptionPane.showMessageDialog(null, "El código ingresado ya existe en la base de datos.\nPor favor intente otro código.");
+                    this.vistaTrabajador.txtCodigo.requestFocus();
+                }
+            }    
+        }
+        // Validación instantanea al salir del campo rut
+        else if(fe.getSource()==this.vistaTrabajador.txtRut){
+            if(!this.vistaTrabajador.txtRut.getText().equals("")){
+                if(!modelo.validarRut(this.vistaTrabajador.txtRut.getText())){
+                    JOptionPane.showMessageDialog(null, "El rut ingresado no es válido.\nPor favor ingrese el rut sin puntos ni dígito verificador.");
+                    this.vistaTrabajador.txtRut.requestFocus();
+                }
+            }    
+        }
+        // Validación instantanea al salir del campo nombre
+        else if(fe.getSource()==this.vistaTrabajador.txtNombre){
+            if(!this.vistaTrabajador.txtNombre.getText().equals("")){
+                if(!modelo.validarLargoString(this.vistaTrabajador.txtNombre.getText())){
+                    JOptionPane.showMessageDialog(null, "El nombre ingresado exede el número máximo de caracteres permitido.\nPor favor ingrese el un nombre de no más de 20 caracteres.");
+                    this.vistaTrabajador.txtNombre.requestFocus();
+                }
+            }    
+        }
+        // Validación instantanea al salir del campo apellido
+        else if(fe.getSource()==this.vistaTrabajador.txtApellido){
+            if(!this.vistaTrabajador.txtApellido.getText().equals("")){
+                if(!modelo.validarLargoString(this.vistaTrabajador.txtApellido.getText())){
+                    JOptionPane.showMessageDialog(null, "El apellido ingresado exede el número máximo de caracteres permitido.\nPor favor ingrese el un apellido de no más de 20 caracteres.");
+                    this.vistaTrabajador.txtApellido.requestFocus();
+                }
+            }    
+        }
+        // Validación instantanea al salir del campo celular
+        else if(fe.getSource()==this.vistaTrabajador.txtCelular){
+            if(!this.vistaTrabajador.txtCelular.getText().equals("")){
+                if(!modelo.validarCelular(this.vistaTrabajador.txtCelular.getText())){
+                    JOptionPane.showMessageDialog(null, "El número celular ingresado no coincide con el formato esperado (9 dígitos).\nPor favor ingrese solo números.");
+                    this.vistaTrabajador.txtCelular.requestFocus();
+                }
+            }    
+        }
+        // Validación instantanea al salir del campo email
+        else if(fe.getSource()==this.vistaTrabajador.txtEmail){
+            if(!this.vistaTrabajador.txtEmail.getText().equals("")){
+                
+                JOptionPane.showMessageDialog(null, "Validación email pendiente.");
+                /*
+                if(!modelo.validarEmail(this.vistaTrabajador.txtEmail.getText())){
+                    JOptionPane.showMessageDialog(null, "El número celular ingresado no coincide con el formato esperado (9 dígitos).\nPor favor ingrese solo números.");
+                    this.vistaTrabajador.txtEmail.requestFocus();
+                }
+                */
+            }    
+        }
+        // Validación instantanea al salir del campo sueldo
+        else if(fe.getSource()==this.vistaTrabajador.txtSueldo){
+            if(!this.vistaTrabajador.txtSueldo.getText().equals("")){
+                if(!modelo.validarSueldo(this.vistaTrabajador.txtSueldo.getText())){
+                    JOptionPane.showMessageDialog(null, "El sueldo ingresado no es válido.\nPor favor ingrese solo valores mayores a $120.000");
+                    this.vistaTrabajador.txtSueldo.requestFocus();
+                }
+            }    
+        }
     }
 }
