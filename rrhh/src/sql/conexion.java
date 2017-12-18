7e691ac960e1a4f3cf4378956797c058a89d75bc
Duoc UC <Duoc UC@B33-15>
2017-12-18 09:30:18 -0300

    agregue 3 variables

--- rrhh/src/sql/conexion.java
+++ rrhh/src/sql/conexion.java
@@ -1,6 +1,25 @@
 package sql;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class conexion {
-    int z = 3;
+    public static Connection getConexion(){
+        
+        Connection connection = null;
+        
+        try {
+            String driverClassName = "com.mysql.jdbc.Driver";
+            String driverUrl = "jdbc:mysql://localhost/rrhh_bd";
+            Class.forName(driverClassName);
+            connection = DriverManager.getConnection(driverUrl, "root","");
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return connection;
+    }    
 }
