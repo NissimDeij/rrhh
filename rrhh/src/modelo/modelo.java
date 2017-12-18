5a270e6fae7176acecfd6cd438a5e248339b1c20
Duoc UC <Duoc UC@B33-15>
2017-12-18 11:30:39 -0300

    ND - agregue validaciones y metodos para agregar y eliminar trabajador

--- rrhh/src/modelo/modelo.java
+++ rrhh/src/modelo/modelo.java
@@ -1,14 +1,50 @@
 package modelo;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Arrays;
+import sql.conexion;
 
-public class modelo {    
+public class modelo extends conexion{    
     
     //metodo para agregar trabajador
-    
+    public boolean agregarTrabajador(
+            int codigo,
+            String rut,
+            String nombre,
+            String apellido,
+            int celular,
+            String email,
+            int sueldoBruto,
+            String estadoCivil,
+            String nomDepto){
+        String query = "INSERT INTO rrhh_bd.empleados("+
+                "codigo,rut,nombre,apellido,celular,email,sueldoBruto,estadoCivil,nomDepto)" +
+                "VALUES('"+codigo+"','"+rut+"','"+nombre+"','"+apellido+"','"+celular+"','"+email+"','"+sueldoBruto+"','"+estadoCivil+"','"+nomDepto+"');";
+        try {
+            PreparedStatement pstm = this.getConexion().prepareStatement(query);
+            pstm.execute();
+            pstm.close();
+            return true;
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
     
     //metodo para eliminar trabajador
-    
+    public boolean eliminarTrabajador(int codigo){
+        String query = "DELETE FROM rrhh_bd.empleados WHERE codigo ="+codigo+";";
+        try {
+            PreparedStatement pstm = this.getConexion().prepareStatement(query);
+            pstm.execute();
+            pstm.close();
+            return true;
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
    
     
     //metodo para validar codigo sea mayor a 0 y menor o igual a 100
@@ -19,6 +55,7 @@ public class modelo {
         return false;
     }
     
+    
     //metodo para validar el largo de los String (para capturar el error de la base de datos si excede 20 caracteres)
     public boolean validarLargoString (String varchar){
         if (varchar.length()<=20){
@@ -26,6 +63,7 @@ public class modelo {
         }
         return false;
     }
+    
     
     //metodo para validar que el digito verificador del rut sea 0-9, K o k
     public boolean validarDigitoVerificador(String digito){
@@ -35,6 +73,7 @@ public class modelo {
         }
         return true;
     }
+    
     
     //metodo para validar que el celular este compuesto por digitos y que sea de largo 9
     public boolean validarCelular(String celular){
@@ -50,9 +89,10 @@ public class modelo {
         return true;
     }
     
+    
     //metodo para validar que el sueldo bruto sea mayor o igual a $120.000
-    public boolean validarSueldo(String sueldo){
-        if (Integer.parseInt(sueldo)>=120000){
+    public boolean validarSueldo(String sueldoBruto){
+        if (Integer.parseInt(sueldoBruto)>=120000){
             return true;
         }
         return false;
